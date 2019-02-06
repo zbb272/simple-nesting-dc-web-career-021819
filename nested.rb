436e465e79f930369474f0e047e79ed3f1862427
Zachary <zbberman@yahoo.com>
2019-02-06 16:12:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,7 +86,7 @@ def adding_matz
      }
 
     programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
-    programmer_hash[:yukihiro_matsumoto]
+    programmer_hash
 end
 
 def changing_alan
