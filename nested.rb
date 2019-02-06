8c7dc8b7e6af81ce36e12ccd85c152d25583ab5c
Zachary <zbberman@yahoo.com>
2019-02-06 16:08:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages][0]
+     programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
